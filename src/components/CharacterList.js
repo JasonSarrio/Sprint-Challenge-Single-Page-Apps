3031101f48c89d8c8529b3dd5c56135fa97919f5
JasonSarrio <jsarrio16@gmail.com>
2020-01-24 18:48:20 -0600

    Working on List.js

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "../components/CharacterCard.js";
+
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
