00a05698ac84eea1f013715238840c292e33ca89
JasonSarrio <jsarrio16@gmail.com>
2020-01-24 18:50:44 -0600

    Working on list doing error catch

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -3,17 +3,24 @@ import axios from "axios";
 import CharacterCard from "../components/CharacterCard.js";
 
 
-export default function CharacterList() {
+export default function CharacterList(props) {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState();
+  const [searchArg, setSearchArg] = useState();
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
-
-  return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-  );
-}
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(res => {
+        console.log(res.data.results);
+        if (!props.searchArg.values || props.searchArg.values.search === "") {
+          setCharacters(res.data.results);
+          console.log("No search");
+        } else {
+          setCharacters(res.data.results.filter((item) => {
+            console.log("filtering", item.name + " === " + props.searchArg.values.search);
+            return item.name.includes(props.searchArg.values.search);
+          }))
+        }
+      })
+      
